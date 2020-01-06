1d73bfc738c8e07cb94d2e27f3ebc1cfe8c7792a
Edmund Chan <edmund@nerb.com.sg>
2020-01-06 14:39:37 +0800

    Fix formatting using Prettier

--- frontend/index.js
+++ frontend/index.js
@@ -2,18 +2,18 @@
   /*******************
    * VARIABLES       *
    ******************/
-  const rootURL = "http://localhost:8888/wp-rest-api-upload-media/backend";
-  const mediaEndpoint = rootURL + "/wp-json/wp/v2/media";
-  const userEndpoint = rootURL + "/wp-json/wp/v2/users/me";
+  const rootURL = 'http://localhost:8888/wp-rest-api-upload-media/backend';
+  const mediaEndpoint = rootURL + '/wp-json/wp/v2/media';
+  const userEndpoint = rootURL + '/wp-json/wp/v2/users/me';
 
-  const wrapper = document.getElementById("profile-wrapper");
-  const form = document.getElementById("profile-form");
-  const name = document.getElementById("name");
-  const description = document.getElementById("description");
-  const profilePic = document.getElementById("profile-pic");
-  const profilePicInput = document.getElementById("profile-pic-input");
-  const spinner = document.getElementById("spinner");
-  const fileLabel = document.querySelector(".custom-file-label");
+  const wrapper = document.getElementById('profile-wrapper');
+  const form = document.getElementById('profile-form');
+  const name = document.getElementById('name');
+  const description = document.getElementById('description');
+  const profilePic = document.getElementById('profile-pic');
+  const profilePicInput = document.getElementById('profile-pic-input');
+  const spinner = document.getElementById('spinner');
+  const fileLabel = document.querySelector('.custom-file-label');
 
   /*******************
    * FUNCTIONS       *
@@ -23,10 +23,10 @@
   function getData() {
     //hide Card display to mimic loading
     wrapper.style.opacity = 0;
-    fetch(userEndpoint + "?context=edit", {
-      method: "GET",
+    fetch(userEndpoint + '?context=edit', {
+      method: 'GET',
       headers: {
-        Authorization: "Basic " + window.btoa("admin:password")
+        Authorization: 'Basic ' + window.btoa('admin:password')
       }
     })
       .then(res => res.json())
@@ -37,7 +37,7 @@
         description.innerHTML = data.description;
         profilePic.innerHTML = data.profile_image
           ? `<img class="card-img-top" src="${data.profile_image}" alt="${data.last_name}" />`
-          : "No Image found";
+          : 'No Image found';
       })
       .catch(err => {
         console.log(err);
@@ -46,9 +46,9 @@
 
   //Toggle spinner
   function toggleSpinner() {
-    return spinner.classList.contains("visible")
-      ? spinner.classList.replace("visible", "invisible")
-      : spinner.classList.replace("invisible", "visible");
+    return spinner.classList.contains('visible')
+      ? spinner.classList.replace('visible', 'invisible')
+      : spinner.classList.replace('invisible', 'visible');
   }
 
   /*******************
@@ -57,24 +57,24 @@
 
   //By default, Bootstrap's custom File input doesn't display the chosen file name.
   //Hence this event handler is required.
-  profilePicInput.addEventListener("change", function(e) {
+  profilePicInput.addEventListener('change', function(e) {
     fileLabel.textContent = e.target.files[0].name;
   });
 
   //Submit image to media library
-  form.addEventListener("submit", function(e) {
+  form.addEventListener('submit', function(e) {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("file", profilePicInput.files[0]);
+    formData.append('file', profilePicInput.files[0]);
     //display spinner
     toggleSpinner();
     //send image to media library
     fetch(mediaEndpoint, {
-      method: "POST",
+      method: 'POST',
       headers: {
         //when using FormData(), the 'Content-Type' will automatically be set to 'form/multipart'
         //so there's no need to set it here
-        Authorization: "Basic " + window.btoa("admin:password")
+        Authorization: 'Basic ' + window.btoa('admin:password')
       },
       body: formData
     })
@@ -85,21 +85,21 @@
         };
         //send image url to backend
         fetch(userEndpoint, {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
-            Authorization: "Basic " + window.btoa("admin:password")
+            'Content-Type': 'application/json',
+            Authorization: 'Basic ' + window.btoa('admin:password')
           },
           body: JSON.stringify(input)
         })
           .then(res => res.json())
           .then(data => {
             //clear file input text content
-            fileLabel.textContent = "";
+            fileLabel.textContent = '';
             //hide spinner
             toggleSpinner();
             //close modal
-            $("#exampleModal").modal("hide");
+            $('#exampleModal').modal('hide');
             //re-fetch data from server
             getData();
           })
